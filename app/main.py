03ae1d664695a7c95d75e8cc76c7daae8f02c572
Steni <steniaalix@gmail.com>
2026-08-23 15:32:52 +0530

    Created a working TWF API

--- app/main.py
+++ app/main.py
@@ -1,8 +1,62 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
 
 app=FastAPI()
 
-@app.get("/predict")
+artifact=joblib.load("app/twf_model.joblib")
+model=artifact["model"]
+encoder=artifact['encoder']
+threshold=artifact['threshold']
+feature_order=artifact['feature_order']
 
-def predict():
-    return {"message":"Hello World"}
+
+class input_template(BaseModel):
+    type:str
+    air_temperature:float
+    process_temperature:float
+    rotational_speed:int
+    torque:float
+    tool_wear:float
+
+
+@app.get("/")
+
+def home():
+    return {
+        "message":"TWF prediction API is running"
+    }
+
+@app.post("/predict")
+
+def predict(data:input_template):
+    encoded_type=encoder.transform([data.type])[0]
+    power=data.torque*data.rotational_speed
+    input_df=pd.DataFrame([{
+        "Type":encoded_type,
+        "Air temperature [K]":data.air_temperature,
+        "Process temperature [K]":data.process_temperature,
+        "Rotational speed [rpm]":data.rotational_speed,
+        "Torque [Nm]":data.torque,
+        "Tool wear [min]":data.tool_wear,
+        "Power":power
+    }])
+
+    input_df=input_df[feature_order]
+
+    prob=model.predict_proba(input_df)[0][1]
+    prediction=int(prob>=threshold)
+
+    if prediction==1:
+        result="Tool Wear Failure predicted"
+    else:
+        result="No Tool Wear Failure predicted"
+
+    return {
+        "prediction":prediction,
+        "result":result,
+        "failure_probability":round(float(prob),4),
+        "optimized_threshold":threshold,
+        "power":round(power,2)
+    }
